d8dbdcd20aea07395a5e78a2ddf90f731bd8b3c1
Eduarda <112082862+eduardamarqueszxd@users.noreply.github.com>
2023-11-27 22:43:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-function tocaSomPom(){
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+const elemento = document.querySelector(seletorAudio);
 
+  if (elemento === null) {
+  //alert('Elemento não encontrado');
+  console.log('elemento não encontrado')
+  }
+if (elemento != null) {
+  elemento.play();
+  }
+}
+
+for(let contador = 0;contador < listaDeTeclas.length;contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+   
+    tecla.onclick = function () {
+      tocaSom(idAudio);
     }
 
-document.querySelector('.tecla_pom');
+    tecla.onkeydown = function (evento) {
+
+      if (evento.code === 'Enter' || evento.code === 'Space' ) {
+        tecla.classList.add('ativa');
+        }
+      
+      }
+
+      tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+        }
+
+}
